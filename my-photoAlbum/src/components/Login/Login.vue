42c28798df8d966bf86764e779feeb3e19ca9e9f
wangtiantian18 <idsaye@163.com>
2020-01-08 10:14:28 +0800

    测试一下

--- my-photoAlbum/src/components/Login/Login.vue
+++ my-photoAlbum/src/components/Login/Login.vue
@@ -9,16 +9,28 @@
       <div class="title">时光相册，欢迎您的登录</div>
       <div class="info">
         <form method="get">
-          <label for="username">用户名:</label>
-          <input id="username" type="text" placeholder="请输入用户名" class="inp" ref="username"/>
-          <br/>
-          <label for="password">密  码:</label>
-          <input id="password" type="password" placeholder="请输入密码" class="inp" ref="password"/>
-          <br/>
+          <div class="tabWay">
+            <span class="wayStyle" :class="wayFlag?'wayStyle2':''" @click="way1">账号登录</span>
+            <span class="wayStyle" :class="!wayFlag?'wayStyle2':''" @click="way2">验证码登录</span>
+          </div>
+          <div class="way1" v-if="wayFlag">
+            <input type="text" placeholder="请输入用户名或邮箱" class="inp" ref="username"/>
+            <br/>
+            <input type="password" placeholder="请输入密码" class="inp" ref="password"/>
+            <br/>
+            <a class="found">忘记密码</a>
+          </div>
+          <div class="way2" v-if="!wayFlag">
+            <input type="text" placeholder="请输入你的邮箱" class="inp" ref="useremail"/>
+            <br/>
+            <input type="text" placeholder="请输入验证码" class="inp inpcode" ref="code"/>
+            <button class="btnCode">获取验证码</button>
+          </div>
           <input class="btn subBtn" type="submit" value="点击登录" @click="submit"/>
           <input class="btn" type="reset" value="取消" @click="reset"/>
         </form>
       </div>
+      <p class="toRegister">没有账号？<a>立即注册</a></p>
     </div>
   </div>
 </template>
@@ -28,12 +40,21 @@
     name: "Login",
     data(){
       return{
+        wayFlag:true,
         bgImg: {
-          backgroundImage: "url(" + require("../../assets/images/login_bg.gif") + ")",
+          backgroundImage: "url(" + require("../../assets/images/005.jpg") + ")",
+          backgroundRepeat:'no-repeat',
+          backgroundSize:'100% 100%'
         },
       }
     },
     methods:{
+      way1(){
+        this.wayFlag=true;
+      },
+      way2(){
+        this.wayFlag=false;
+      },
       submit(){
         console.log('点击登录');
       },
@@ -48,18 +69,19 @@
 <style lang="scss" scoped>
   .login {
     height: 100%;
+    background: #000;
     .loginBox{
       position: absolute;
       top: 50%;
       left: 50%;
       transform: translate(-50%,-70%);
       width: 500px;
-      height: 350px;
+      height: 450px;
       border-radius: 10px;
       background: #FFFFFF;
       .title{
-        height: 150px;
-        line-height: 150px;
+        height: 100px;
+        line-height: 100px;
         font-size: 32px;
         font-weight: bold;
         text-align: center;
@@ -69,33 +91,70 @@
         color: transparent;
       }
       .info{
-        height: 200px;
+        height: 140px;
         padding: 0 50px;
         form{
-          label{
-            display: inline-block;
-            height: 50px;
+          .tabWay{
+            height: 30px;
+            margin-bottom: 20px;
+            line-height: 30px;
+            text-align: center;
             font-size: 20px;
+            .wayStyle{
+              display: inline-block;
+              cursor: pointer;
+              &:first-child{
+                margin-right: 50px;
+              }
+            }
+            .wayStyle2{
+              color: deepskyblue;
+              border-bottom: 2px solid deepskyblue;
+            }
           }
           .inp{
             position: relative;
-            top: -13px;
             height: 30px;
-            width: 300px;
+            width: 100%;
             //去除默认样式
             border: 0px;
             outline: none;
             background:none;
             //添加自己的样式
-            border-bottom: 3px solid paleturquoise;
+            border: 2px solid #dcdcdc;
             text-align: center;
             font-size: 16px;
+            margin-bottom: 20px;
             &::-webkit-input-placeholder{
               color: #D8D8D8;
             }
             &:focus{
-              border-bottom: 3px solid green;
+              border: 2px solid green;
             }
+          }
+          .inpcode{
+            width: 278px;
+          }
+          .way1{
+            position: relative;
+            .found{
+              position: absolute;
+              right: 0;
+              bottom: 20px;
+              cursor: pointer;
+              height: 20px;
+              &:hover{
+                color: deepskyblue;
+              }
+            }
+          }
+          .btnCode{
+            height: 40px;
+            width: 110px;
+            border-radius: 20px;
+            cursor: pointer;
+            border: none;
+            background: #d8d8d8;
           }
           #password{
             margin-left: 9px;
@@ -104,7 +163,8 @@
             height: 40px;
             width: 110px;
             background: gainsboro;
-            border: 0px;
+            border: none;
+            outline: none;
             border-radius: 20px;
             font-size: 18px;
             cursor: pointer;
@@ -115,10 +175,21 @@
           }
           .subBtn{
             width: 250px;
-            margin-right: 20px;
+            margin-right: 32px;
             background: orange;
             color: #fff;
           }
+        }
+      }
+      .toRegister{
+        text-align: right;
+        padding-right: 50px;
+        margin-top: 120px;
+        height: 30px;
+        line-height: 30px;
+        a{
+          color: deepskyblue;
+          cursor: pointer;
         }
       }
     }
--- my-photoAlbum/src/components/Register/Register.vue
+++ my-photoAlbum/src/components/Register/Register.vue
@@ -4,17 +4,44 @@
  * Description: 注册页面
  -->
 <template>
-    <div>
-      注册页
+    <div class="register" :style="bgImg">
+      <div class="registerBox">
+        <div class="title">时光相册</div>
+      </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Register"
+      name: "Register",
+      data(){
+        return{
+          bgImg: {
+            backgroundImage: "url(" + require("../../assets/images/002.png") + ")",
+          },
+        }
+      }
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+  .register{
+    height: 100%;
+    background: paleturquoise;
+    .registerBox{
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      width: 500px;
+      height: 400px;
+      transform: translate(-50%,-50%);
+      background: #fff;
+      border-radius: 10px;
+      .title{
+        height: 100px;
+        line-height: 100px;
+        text-align: center;
+      }
+    }
+  }
 </style>
